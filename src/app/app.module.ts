873a3fa1b240f0bb9ea4c9748cfed4e71b7abaf9
degtyar <firstyuriy@gmail.com>
2017-11-25 13:37:50 +0200

    chore: add service workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
-
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from "../environments/environment"
 
 import {AppComponent} from './app.component';
-import { ImageCardComponent } from './image-card/image-card.component';
+import {ImageCardComponent} from './image-card/image-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageCardComponent
+    ImageCardComponent,
   ],
   imports: [
     BrowserModule,
     MatToolbarModule,
     MatIconModule,
-    MatCardModule
+    MatCardModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
